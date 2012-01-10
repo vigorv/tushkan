805c4eeda6a2c9b291b36fba727e30ac29d9fc77
DiamondSnow <snow@diamondc.ru>
2012-01-10 09:12:14 +0700

    Controller update

--- protected/components/Controller.php
+++ protected/components/Controller.php
@@ -1,14 +1,31 @@
 <?php
-class Controller extends CController
-{
-	public $layout='//layouts/index';
 
-	public $menu = array();
+class Controller extends CController {
 
-	public $breadcrumbs=array();
+    public $layout = '//layouts/index';
+    public $menu = array();
+    public $breadcrumbs = array();
 
-	public function __construct()
-	{
-		Yii::app()->setLanguage('ru');
-	}
+    public function init() {
+        parent::init();
+        $app = Yii::app();
+        if (isset($_GET['_lang'])) {
+            $app->language = $_GET['_lang'];
+            $app->session['_lang'] = $app->language;
+        } else if (isset($app->session['_lang'])) {
+            $app->language = $app->session['_lang'];
+        }
+    }
+
+    public function beforeAction($action) {       
+        if (Yii::app()->request->isAjaxRequest) {
+            //    $this->renderPartial('_ajaxContent', $data);
+            $this->layout = 'ajax';
+        } else {
+            //$this->layout='index';
+            // $this->render('index', $data);
+        }
+        return true;
+    }
+
 }

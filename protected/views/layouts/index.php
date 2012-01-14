2e7f1b61531f9b0eaf12c8941b7c9c9fa5151d14
Snow <snowcanbe@gmail.com>
2012-01-14 12:36:52 +0700

    resolved conflict

--- protected/views/layouts/index.php
+++ protected/views/layouts/index.php
@@ -1,75 +1,75 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ru" lang="ru">
-<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<meta name="language" content="ru" />
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="language" content="ru" />
 
-	<!-- blueprint CSS framework -->
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/screen.css" media="screen, projection" />
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/print.css" media="print" />
-	<!--[if lt IE 8]>
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/ie.css" media="screen, projection" />
-	<![endif]-->
+        <!-- blueprint CSS framework -->
+        <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/screen.css" media="screen, projection" />
+        <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/print.css" media="print" />
+        <!--[if lt IE 8]>
+        <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/ie.css" media="screen, projection" />
+        <![endif]-->
+        <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css" />
+        <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css" />
+        <title><?php echo CHtml::encode($this->pageTitle); ?></title>
+        <?php
+        Yii::app()->getClientScript()->registerCoreScript('jquery');
+        Yii::app()->getClientScript()->registerScriptFile(Yii::app()->request->baseUrl . "/js/jquery-ui/js/jquery-ui-1.8.16.custom.min.js");
+        Yii::app()->getClientScript()->registerCssFile(Yii::app()->request->baseUrl . "/js/jquery-ui/css/pepper-grinder/jquery-ui-1.8.16.custom.css");
+        ?>
+        <title><?php echo CHtml::encode($this->pageTitle); ?></title>
+    </head>
+    <body>
+        <div class="container" id="page">
 
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css" />
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css" />
+            <div id="header">
+                <div id="logo"><?php echo CHtml::encode(Yii::app()->name); ?></div>
+            </div><!-- header -->
 
-	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
-<?php
-	Yii::app()->getClientScript()->registerCoreScript('jquery');
-	Yii::app()->getClientScript()->registerScriptFile(Yii::app()->request->baseUrl . "/js/jquery-ui/js/jquery-ui-1.8.16.custom.min.js");
-	Yii::app()->getClientScript()->registerCssFile(Yii::app()->request->baseUrl . "/js/jquery-ui/css/pepper-grinder/jquery-ui-1.8.16.custom.css");
-?>
-</head>
+            <div id="mainmenu">
+                <?php
+                $this->widget('zii.widgets.CMenu', array(
+                    'items' => array(
+                        array('label' => 'Home', 'url' => array('/')),
+                        array('label' => Yii::t('common', 'Registration'), 'url' => array('/register'), 'visible' => Yii::app()->user->isGuest),
+                        array('label' => Yii::t('users', 'Fill up balance'), 'url' => array('/pays'), 'visible' => !Yii::app()->user->isGuest),
+                        array('label' => Yii::t('common', 'Login'), 'url' => array('/register/login'), 'visible' => Yii::app()->user->isGuest),
+                        array('label' => Yii::t('common', 'Logout') . ' (' . Yii::app()->user->name . ')',
+                            'url' => array('/register/logout'), 'visible' => !Yii::app()->user->isGuest
+                        ),
+                        array('label' => Yii::t('common', 'Admin index'), 'url' => array('/admin'), 'visible' => (Yii::app()->user->getState('dmUserPower') >= _IS_MODERATOR_))
+                    ),
+                ));
+                ?>
+            </div><!-- mainmenu -->
+            <?php if (isset($this->breadcrumbs)): ?>
+                <?php
+                $this->widget('zii.widgets.CBreadcrumbs', array(
+                    'links' => $this->breadcrumbs,
+                ));
+                ?><!-- breadcrumbs -->
+            <?php endif ?>
+            <?php if (Yii::app()->user->hasFlash('success')): ?>
+                <div class="flash-notice">
+                    <?php echo Yii::app()->user->getFlash('success') ?>
+                </div>
+            <?php endif ?>
+            <?php if (Yii::app()->user->hasFlash('error')): ?>
+                <div class="flash-error">
+                    <?php echo Yii::app()->user->getFlash('error') ?>
+                </div>
+            <?php endif ?>
 
-<body>
+            <?php echo $content; ?>
 
-<div class="container" id="page">
+            <div id="footer">
+                Copyright &copy; <?php echo date('Y'); ?> <?php echo CHtml::encode(Yii::app()->name); ?><br/>
+                All Rights Reserved.<br/>
+                <?php //echo Yii::powered();   ?>
+            </div><!-- footer -->
 
-	<div id="header">
-		<div id="logo"><?php echo CHtml::encode(Yii::app()->name); ?></div>
-	</div><!-- header -->
+        </div><!-- page -->
 
-	<div id="mainmenu">
-		<?php $this->widget('zii.widgets.CMenu',array(
-			'items'=>array(
-				array('label'=>'Home', 'url'=>array('/')),
-				array('label'=>Yii::t('common', 'Registration'), 'url'=>array('/register'), 'visible'=>Yii::app()->user->isGuest),
-				array('label'=>Yii::t('users', 'Fill up balance'), 'url'=>array('/pays'), 'visible'=>!Yii::app()->user->isGuest),
-				array('label'=>Yii::t('common', 'Login'), 'url'=>array('/register/login'), 'visible'=>Yii::app()->user->isGuest),
-				array('label'=>Yii::t('common', 'Logout') . ' ('.Yii::app()->user->name.')',
-					'url'=>array('/register/logout'), 'visible'=>!Yii::app()->user->isGuest
-				),
-				array('label'=> Yii::t('common', 'Admin index'), 'url'=>array('/admin'), 'visible'=>(Yii::app()->user->getState('dmUserPower') >= _IS_MODERATOR_))
-			),
-		)); ?>
-	</div><!-- mainmenu -->
-	<?php if(isset($this->breadcrumbs)):?>
-		<?php $this->widget('zii.widgets.CBreadcrumbs', array(
-			'links'=>$this->breadcrumbs,
-		)); ?><!-- breadcrumbs -->
-	<?php endif?>
-
-	<?php if(Yii::app()->user->hasFlash('success')):?>
-	<div class="flash-notice">
-	<?php echo Yii::app()->user->getFlash('success')?>
-	</div>
-	<?php endif?>
-	<?php if(Yii::app()->user->hasFlash('error')):?>
-	<div class="flash-error">
-	<?php echo Yii::app()->user->getFlash('error')?>
-	</div>
-	<?php endif?>
-
-	<?php echo $content; ?>
-
-	<div id="footer">
-		Copyright &copy; <?php echo date('Y'); ?> <?php echo CHtml::encode(Yii::app()->name); ?><br/>
-		All Rights Reserved.<br/>
-		<?php //echo Yii::powered(); ?>
-	</div><!-- footer -->
-
-</div><!-- page -->
-
-</body>
+    </body>
 </html>

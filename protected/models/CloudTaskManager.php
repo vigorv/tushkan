e3589bb18b2981b2ea27f99e8d2a23288b63569b
Snow <snowcanbe@gmail.com>
2012-02-09 13:01:15 +0700

    CloudTaskManager update

--- protected/models/CloudTaskManager.php
+++ protected/models/CloudTaskManager.php
@@ -7,8 +7,6 @@ class CloudTaskManager {
     private static $_models = array();
 # array with the options to create stream context
 
-    
-    
     public static function model($className=__CLASS__) {
 	if (isset(self::$_models[$className]))
 	    return self::$_models[$className];
@@ -19,17 +17,25 @@ class CloudTaskManager {
     }
 
     public function CreateFileTask($queue_id, $fid, $preset_name) {
-	$zone=0;
-	$server=CServers::model()->getServer($TASK_SERVER,$zone);
-	$id = CServers::model()->sendCommandAddr('/task/create',$server,array($queue_id,$fid,$preset_name));
-	return $id;
+	$zone = 0;
+	$server = CServers::model()->getServer($TASK_SERVER, $zone);
+	if ($server) {
+	    $id = CServers::model()->sendCommandAddr('/task/create', $server, array($queue_id, $fid, $preset_name));
+	    return $id;
+	}
+	return false;
     }
 
     public function AbortFileTask($queue_id, $task_id) {
-	$zone=0;
-	$server=CServers::model()->getServer($TASK_SERVER,$zone);
-	$result = CServers::model()->sendCommandAddr('/task/abort',$server,array($queue_id,$fid,$preset_name));
-	return $result;
+	$zone = 0;
+	$server = CServers::model()->getServer($TASK_SERVER, $zone);
+	if ($server) {
+	    $result = CServers::model()->sendCommandAddr('/task/abort', $server, array($queue_id, $fid, $preset_name));
+	    return $result;   
+	} else
+	     return false;
+	
+	
     }
 
 }

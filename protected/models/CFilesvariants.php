d0f3f2adba9eb4a1d9ac9fdf7ff0b14c5c3dc1e1
Snow <snowcanbe@gmail.com>
2012-02-20 17:05:09 +0700

    filevariant class

---
+++ protected/models/CFilesvariants.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * @property $id
+ * @property $file_id
+ * @property $preset_id
+ * @property $fsize
+ * @property $fmd5
+ */
+class CFilevariants extends CActiveRecord {
+
+    /**
+     *
+     * @param string $className
+     * @return CFileVariants
+     */
+    public static function model($className = __CLASS__) {
+	return parent::model($className);
+    }
+
+    public function tableName() {
+	return '{{files_variants}}';
+    }
+
+}

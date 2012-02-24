f5e2bd8b852843248acddd77a145676e5c50e270
Snow <snowcanbe@gmail.com>
2012-02-24 16:08:14 +0700

    UserObjects class

---
+++ protected/models/CUserObjects.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * ActiveRecord class for UserObjects
+ * 
+ * @property id
+ * @property title
+ * @property user_id
+ * @property type_id
+ * @property active
+ * @property parent_id
+ */
+class CUserObjects extends CActiveRecord {
+    
+    /**
+     *
+     * @param string $className
+     * @return CUserObjects
+     */
+    public static function model($className = __CLASS__) {
+	return parent::model($className);
+    }
+
+    public function tableName() {
+	return '{{userobjects}}';
+    }
+
+}

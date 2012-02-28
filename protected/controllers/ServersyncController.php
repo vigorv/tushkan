b6f2cdf4ce7d93e964598c05e5b8527d6f67c5f2
Snow <snowcanbe@gmail.com>
2012-02-28 12:03:09 +0700

    devices class

--- protected/controllers/ServersyncController.php
+++ protected/controllers/ServersyncController.php
@@ -114,7 +114,7 @@ class ServersyncController extends Controller {
 		    else
 			$result = array('error' => "Can't save record");
 		} else
-		    $result = array('error' => "Unsupported filetype");
+		    $result = array('error' => "Unsupported filetype",'error_code'=> 1);
 	    } else
 		$result = array('error' => 'Bad input data');
 	    echo serialize($result);

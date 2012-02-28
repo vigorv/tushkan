ab56c117ba745c7acb14eca64fc83a43c21ef7dc
Snow <snowcanbe@gmail.com>
2012-02-28 10:23:29 +0700

    admin menu link

--- protected/views/layouts/admin.php
+++ protected/views/layouts/admin.php
@@ -39,6 +39,8 @@
                     <li><a href="<?php echo $this->createUrl('products/admin'); ?>"><?php echo Yii::t('common', 'Products');?></a></li>
                     <li><a href="<?php echo $this->createUrl('pages/admin'); ?>"><?php echo Yii::t('common', 'Pages');?></a></li>
                     <li><a href="<?php echo $this->createUrl('personaldataparams/admin'); ?>"><?php echo Yii::t('params', 'Personal data params');?></a></li>
+                    <li><a href="<?php echo $this->createUrl('fileservers/admin'); ?>"><?php echo Yii::t('admin', 'FileServers');?></a></li>
+                    <li><a href="<?php echo $this->createUrl('Zones/admin'); ?>"><?php echo Yii::t('admin', 'Zones');?></a></li>
                 </ul>
             </div>
             <div id="admincontent">

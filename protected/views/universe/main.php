2e97a520541059ee80976c9cf98cfe44597eaa83
Snow <snowcanbe@gmail.com>
2012-03-02 10:09:07 +0700

    template

--- protected/views/universe/main.php
+++ protected/views/universe/main.php
@@ -1,22 +1,6 @@
-<div id="m_panel">
-
-</div>
-<div id="m_goods">
-
-</div>
-<div id="m_lib">
-
-</div>
-<div id="m_device">
-    
-</div>
 
 <script langauge="javascript">
     $('#m_panel').load('/universe/panel');
     $('#m_goods').load('/universe/goods');
     $('#m_lib').load('/universe/library');
-  //$('#device_content').load('devices/view');
-    //$('#section_content').load('sections/view')
-//   $('#section_files').load('files')
-   //$('#upload_block').load('/universe/upload')
 </script>

8d975267a79a3a988b77bb3d984af0aec9deeb9a
Snow <snowcanbe@gmail.com>
2012-03-13 15:51:37 +0700

    UserUploads

---
+++ protected/models/CUploads.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * ActiveRecord class for User Uploads
+ * 
+ * @property $id
+ * @property $user_id
+ * @property $filename
+ * @property $kpt
+ * @property $started
+ */
+class CUploads extends CActiveRecord {
+
+	
+
+}

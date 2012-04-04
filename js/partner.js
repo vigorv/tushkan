8f8734b3bdb0339ed18baabc3d36ac020c56aefa
DiamondVano <vano@diamondc.ru>
2012-04-04 16:58:08 +0600

    долил

--- js/partner.js
+++ js/partner.js
@@ -1,9 +1,18 @@
 var imgObject;
+var cloudUrl = "http://mycloud.anka.ws";
 function addToCloud(img, pid, oid, vid)
 {
 	imgObject = img;
-	$.post("http://tushkan/products/Addtoqueue", {partner_id: pid, original_id: oid, original_variant_id: vid}, function(){
-		img = $(imgObject);
-		img.attr("src", img.attr("src") + "/flg/1");
+
+	$.get(cloudUrl + "/products/addtoqueue", {partner_id: pid, original_id: oid, original_variant_id: vid}, function(answer){
+		//id = parseInt(answer);
+		//if (id > 0)
+//		{
+	//		location.href = cloudUrl + "/universe/tview/" + id;
+		//}
+		//alert(answer);
+		//img = $(imgObject).firstChild();
+		//img.attr("src", img.attr("src") + "/flg/1");
 	});
+	return false;
 }

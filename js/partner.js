f2e745d0725604278adb475129eaf6c8f51258f7
DiamondVano <vano@diamondc.ru>
2012-04-05 11:10:34 +0600

    обработка ответа во фрейме

--- js/partner.js
+++ js/partner.js
@@ -1,10 +1,16 @@
 var imgObject;
 var cloudUrl = "http://mycloud.anka.ws";
+var iFrameResult;
 function addToCloud(img, pid, oid, vid)
 {
 	imgObject = img;
 
-	$.get(cloudUrl + "/products/addtoqueue", {partner_id: pid, original_id: oid, original_variant_id: vid}, function(answer){
+	iFrameResult = "";
+	f = $("cloudframe");
+	f.html("");
+	f.attr("src", cloudUrl + "/products/addtoqueue/partner_id/" + pid + "/original_id/" + oid + "/original_variant_id/" + vid);
+
+//	$.get(cloudUrl + "/products/addtoqueue", {partner_id: pid, original_id: oid, original_variant_id: vid}, function(answer){
 		//id = parseInt(answer);
 		//if (id > 0)
 //		{
@@ -13,6 +19,18 @@ function addToCloud(img, pid, oid, vid)
 		//alert(answer);
 		//img = $(imgObject).firstChild();
 		//img.attr("src", img.attr("src") + "/flg/1");
-	});
+//	});
 	return false;
+}
+
+function cloudFrameResult()
+{
+	f = $("cloudframe");
+	iFrameResult = f.html();
+	if (iFrameResult)
+	{
+		alert(iFrameResult);
+		return;
+	}
+	window.setTimeOut("cloudFrameResult()", 500);
 }

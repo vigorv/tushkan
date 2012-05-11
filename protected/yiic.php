d1906ae4cb39e8eac6581900916130047218db71
DiamondVano <vano@diamondc.ru>
2012-05-11 12:10:05 +0600

    долил

--- protected/yiic.php
+++ protected/yiic.php
@@ -1,10 +1,10 @@
 <?php
 
 // change the following paths if necessary
-if (file_exists(dirname(__FILE__).'/framework'))
-	$yiic=dirname(__FILE__).'/framework/yiic.php';
-else
+if (file_exists(dirname(__FILE__).'/../framework'))
 	$yiic=dirname(__FILE__).'/../framework/yiic.php';
+else
+	$yiic=dirname(__FILE__).'/../../framework/yiic.php';
 $config=dirname(__FILE__).'/config/console.php';
 
 require_once($yiic);

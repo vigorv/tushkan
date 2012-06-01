54a9e8930ca6538e34b0c87b308130d8bfa375af
snow <snow@diamondc.ru>
2012-06-01 16:00:30 +0700

    change list to vitrin list

--- protected/models/CUserObjects.php
+++ protected/models/CUserObjects.php
@@ -2,7 +2,7 @@
 
 /**
  * ActiveRecord class for UserObjects
- * 
+ *
  * @property $id
  * @property $title
  * @property $user_id
@@ -10,47 +10,69 @@
  * @property $active
  * @property $parent_id
  */
-class CUserObjects extends CActiveRecord {
+class CUserObjects extends CActiveRecord
+{
 
     /**
      *
      * @param string $className
      * @return CUserObjects
      */
-    public static function model($className = __CLASS__) {
-	return parent::model($className);
+    public static function model($className = __CLASS__)
+    {
+        return parent::model($className);
     }
 
-    public function tableName() {
-	return '{{userobjects}}';
+    public function tableName()
+    {
+        return '{{userobjects}}';
     }
 
-    public function getList($user_id, $type_id=-1, $page=1, $count=100) {
-	$offset = ($page - 1) * $count;
-	if ($type_id >= 0) {
-	    $type_str = ' AND uo.type_id=' . $type_id;
-	} else
-	    $type_str = '';
-	return Yii::app()->db->createCommand()
-			->select('uo.title,uo.id')
-			->from('{{userobjects}} uo')
-			->where('uo.user_id =' . $user_id . $type_str)
-			->limit($count, $offset)
-			->queryAll();
+    public function getList($user_id, $type_id = -1, $page = 1, $count = 100)
+    {
+        $offset = ($page - 1) * $count;
+        if ($type_id >= 0) {
+            $type_str = ' AND uo.type_id=' . $type_id;
+        } else
+            $type_str = '';
+        return Yii::app()->db->createCommand()
+            ->select('uo.title,uo.id')
+            ->from('{{userobjects}} uo')
+            ->where('uo.user_id =' . $user_id . $type_str)
+            ->limit($count, $offset)
+            ->queryAll();
     }
 
-    public function getObjectsLike($user_id, $like, $page=1, $per_page=10,$type_id=-1) {
-	$offset = ($page - 1) * $per_page;
-	if ($type_id >= 0) {
-	    $type_str = ' AND uo.type_id=' . $type_id;
-	} else
-	    $type_str = '';
-	return Yii::app()->db->createCommand()
-			->select('uo.title,uo.id')
-			->from('{{userobjects}} uo')
-			->where('uo.user_id =' . $user_id .' AND uo.title LIKE "%'.$like.'%"' . $type_str)
-			->limit($per_page, $offset)
-			->queryAll();
+    public function getObjectsLike($user_id, $like, $page = 1, $per_page = 10, $type_id = -1)
+    {
+        $offset = ($page - 1) * $per_page;
+        if ($type_id >= 0) {
+            $type_str = ' AND uo.type_id=' . $type_id;
+        } else
+            $type_str = '';
+        return Yii::app()->db->createCommand()
+            ->select('uo.title,uo.id')
+            ->from('{{userobjects}} uo')
+            ->where('uo.user_id =' . $user_id . ' AND uo.title LIKE "%' . $like . '%"' . $type_str)
+            ->limit($per_page, $offset)
+            ->queryAll();
+    }
+
+    public function getVtrList($user_id, $type_id = -1, $page = 1, $count = 100){
+        $offset = ($page - 1) * $count;
+        if ($type_id >= 0) {
+            $type_str = ' AND pv.type_id=' . $type_id;
+        } else
+            $type_str = '';
+        return Yii::app()->db->createCommand()
+            ->select('tf.title,tf.id')
+            ->from('{{typedfiles}} tf')
+            ->join('{{product_variants}} pv','pv.id = tf.variant_id')
+            ->where('tf.user_id =' . $user_id . $type_str)
+            ->limit($count, $offset)
+            ->queryAll();
+
+
     }
 
 }

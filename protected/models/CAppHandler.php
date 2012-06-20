169b51c4e27f73ca589728baa42bcf447e08498e
snow <snow@diamondc.ru>
2012-06-20 16:51:18 +0700

    check user_id on search cloud_id

--- protected/models/CAppHandler.php
+++ protected/models/CAppHandler.php
@@ -193,7 +193,7 @@ class CAppHandler
             // 10 - poster
                 ->leftJoin('{{variant_qualities}} vq', ' vq.variant_id = pv.id')
                 ->leftJoin('{{product_descriptions}} pd', 'pd.product_id = pv.product_id')
-                ->leftJoin('{{typedfiles}} tf', 'tf.variant_id = pv.id and tf.variant_quality_id = (select max(tf.variant_quality_id) from {{typedfiles}} tf WHERE tf.variant_id = pv.id Limit 1)' )
+                ->leftJoin('{{typedfiles}} tf', 'tf.variant_id = pv.id and tf.variant_quality_id = (select max(tf.variant_quality_id) from {{typedfiles}} tf WHERE tf.variant_id = pv.id Limit 1)  AND tf.user_id = '.Yii::app()->user->id )
                 ->join('{{product_files}} pf', 'pf.variant_quality_id = vq.id and pf.preset_id = 2')
                 ->where('pv.id = :variant_id', array(':variant_id'=>$item_id))
                 ->limit(1)->query();

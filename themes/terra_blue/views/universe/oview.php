4bc7b37385311c195f07e32c85a18c43b06f2665
DiamondVano <vano@diamondc.ru>
2012-09-20 15:04:24 +0600

    долил

--- themes/terra_blue/views/universe/oview.php
+++ themes/terra_blue/views/universe/oview.php
@@ -266,7 +266,7 @@ if (Yii::app()->user->getId() == 2)
 			   $("#flowplayerdiv").modal("show");
 			   $(".close").click(function(){
 			   		$("#flowplayerdiv").modal("hide");
-			   		$("#flowplayerdiv").remove();
+			   		$f().stop();
 			   });
 			});
 

3e2b44f2613912a06e232ec3b34011ece1ad16a5
DiamondVano <vano@diamondc.ru>
2012-09-24 18:06:57 +0600

    долил

--- themes/terra_blue/views/universe/devices.php
+++ themes/terra_blue/views/universe/devices.php
@@ -4,13 +4,13 @@
 //ОРИГИНАЛЬНАЯ ВЕРСТКА
 	echo '
 				<div class="span2 margin-left-only">
-					<a class="device ipad" href="/pages/3/#devices">iPad 3</a>
+					<a class="device ipad" href="/pages/3#devices">iPad 3</a>
 				</div>
 				<div class="span2 margin-left-only">
-					<a class="device ipod-shuffle" href="/pages/3/#devices">iPod</a>
+					<a class="device ipod-shuffle" href="/pages/3#devices">iPod</a>
 				</div>
 				<div class="span2 margin-left-only">
-					<a class="device iphone" href="/pages/3/#devices">Мобильный</a>
+					<a class="device iphone" href="/pages/3#devices">Мобильный</a>
 				</div>
 	';
 //*/
